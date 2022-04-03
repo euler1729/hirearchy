13cd139e1129b5c4d9fe5efa6acb5b8257e7552d
N-Crypt3d <89503458+N-Crypt3d@users.noreply.github.com>
2022-04-03 23:50:52 +0600

    Register as and Location Dropdown added

--- src/main/java/com/example/hirearchy/controller/RegisterAndLoginPageController.java
+++ src/main/java/com/example/hirearchy/controller/RegisterAndLoginPageController.java
@@ -1,17 +1,25 @@
 package com.example.hirearchy.controller;
 
 import com.example.hirearchy.App;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class RegisterAndLoginPageController {
+public class RegisterAndLoginPageController implements Initializable {
+
+    // Scene transition
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -32,5 +40,26 @@ public class RegisterAndLoginPageController {
         stage.setTitle("Register to Hirearchy");
         stage.setScene(scene);
         stage.show();
+    }
+
+    // dropdown options for register as
+
+    @FXML
+    public ComboBox<String> RegisterAsDropdown = new ComboBox<>();
+    public ComboBox<String> LocationDropdown = new ComboBox<>();
+
+    ObservableList<String> RegisterAsOptionsList = FXCollections.observableArrayList("Customer",
+                                                                                    "Electrician", "Plumber",
+                                                                                    "Painter", "Driver",
+                                                                                    "Mechanic");
+    ObservableList<String> LocationsList = FXCollections.observableArrayList("Mirpur",
+            "Gabtoli", "Shyamoli",
+            "Dhanmondi", "Gulshan",
+            "Banani", "Mohammadpur", "Nilkhet", "Banasri", "Kamalapur", "Khilgaon", "Farmgate", "Shahbag");
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        RegisterAsDropdown.setItems(RegisterAsOptionsList);
+        LocationDropdown.setItems(LocationsList);
     }
 }
