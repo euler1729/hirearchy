f88096e3015772bd2a925ce9e4d1c3ed3c08f88f
N-Crypt3d <89503458+N-Crypt3d@users.noreply.github.com>
2022-06-25 00:26:50 +0600

    (Final)Side bar Controller for Customer Home Page

---
+++ src/main/java/com/example/hirearchy/controller/sideBarController.java
@@ -0,0 +1,56 @@
+package com.example.hirearchy.controller;
+
+import com.example.hirearchy.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class sideBarController {
+    private ActionEvent event;
+
+    @FXML
+    private void onHireButtonClick (ActionEvent event){
+        loadOption("Customer", event);
+    }
+
+    @FXML
+    private void onProfileButtonClick (ActionEvent event){
+        loadOption("profile", event);
+    }
+
+    @FXML
+    public void onFaqButtonClick(ActionEvent event){
+        loadOption("faq", event);
+    }
+
+    @FXML
+    private void onAboutButtonClick (ActionEvent event){
+        loadOption("about", event);
+    }
+
+    @FXML
+    private void onLogoutButtonClick(ActionEvent event){
+        loadOption("LoginPage", event);
+    }
+
+    private void loadOption(String opt, ActionEvent event) {
+        Scene scene;
+        Stage stage;
+        FXMLLoader fxmlLoader;
+        try{
+            fxmlLoader = new FXMLLoader(App.class.getResource(opt + ".fxml"));
+            scene = new Scene(fxmlLoader.load());
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
